ceb32b2abdb032fd9088b2823d13fd1626df12f1
Gowtham R <69963862+Gowtham-R-2002@users.noreply.github.com>
2023-03-26 18:31:33 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-$('.nav-item').mouseenter(function(){
-    $(this).css('background-color','purple');
-    $(this).find('.nav-link').css('color','white');
-    $('.nav-link').css('opacity','0.5') ;
-    $(this).find('.nav-link').css('opacity','1'); 
+$(document).ready(function(){
+
+
+
+
+    $('.animation-intro').css('display','');
+    $('.animation-intro').slideDown(3000);
+
+    $('.nav-item').mouseenter(function(){
+        $(this).css('background-color','purple');
+        $(this).find('.nav-link').css('color','white');
+        $('.nav-link').css('opacity','0.5') ;
+        $(this).find('.nav-link').css('opacity','1'); 
+    
+    })
+    
+    $('.nav-item').mouseleave(function(){
+        $(this).css('background-color','');
+        $(this).find('.nav-link').css('color','');
+        $('.nav-link').css('opacity','') ;
+        $(this).find('.nav-link').css('opacity',''); 
+    })
 
 })
 
-$('.nav-item').mouseleave(function(){
-    $(this).css('background-color','');
-    $(this).find('.nav-link').css('color','');
-    $('.nav-link').css('opacity','') ;
-    $(this).find('.nav-link').css('opacity',''); 
-})
+
+
+
 
