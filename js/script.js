0f74485919f504a748ecd729e1f1125b65cc98be
Gowtham R <69963862+Gowtham-R-2002@users.noreply.github.com>
2023-03-26 18:16:21 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('.nav-item').mouseenter(function(){
+    $(this).css('background-color','purple');
+    $(this).find('.nav-link').css('color','white');
+    $('.nav-link').css('opacity','0.5') ;
+    $(this).find('.nav-link').css('opacity','1'); 
+
+})
+
+$('.nav-item').mouseleave(function(){
+    $(this).css('background-color','');
+    $(this).find('.nav-link').css('color','');
+    $('.nav-link').css('opacity','') ;
+    $(this).find('.nav-link').css('opacity',''); 
+})
+
